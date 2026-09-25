4ea4a05a8894f17530fb7974cb0c29d2898f89be
ajay kale <ajaykale749810@gmail.com>
2026-09-25 09:57:07 +0530

    hands on the lambda with map, filter

--- Lambda/lambda_filter.py
+++ Lambda/lambda_filter.py
@@ -1,10 +1,17 @@
-# numbers = [1,2,3,4,5]
+# map
+numbers = [1,2,3,4,5]
+result = list(map(lambda x: x*2, numbers))
+print(result)
 
-# result = list(map(lambda x: x*2, numbers))
-# print(result)
-
-
+# map
 prices = [100, 200, 300, 400]
 discounted = list(map(lambda price: price * 0.90, prices))
 print(discounted)
+
+# filter
+prices = [100, 200, 300, 400, 500, 600]
+expensive = list(
+    filter(lambda price: price >= 250, prices)
+)
+print("expensive",expensive)
 
