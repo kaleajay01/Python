dd70d749ea8dd7742b6d36c89847fc648fbf328e
ajay kale <ajaykale749810@gmail.com>
2026-09-25 09:53:21 +0530

    hands on the lambda with map, filter

---
+++ Lambda/lambda_filter.py
@@ -0,0 +1,10 @@
+# numbers = [1,2,3,4,5]
+
+# result = list(map(lambda x: x*2, numbers))
+# print(result)
+
+
+prices = [100, 200, 300, 400]
+discounted = list(map(lambda price: price * 0.90, prices))
+print(discounted)
+
---
+++ Lambda/lambda_map.py
@@ -0,0 +1,13 @@
+# square
+square = lambda x: x * x
+print(square(2))
+
+
+# addition
+add = lambda a, b: a + b
+print(add(12,12))
+
+# number is even or odd
+even = lambda x: x % 2 == 0
+print(even(22))
+
